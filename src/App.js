310a7b7ebcda76a192953933db83b453a9f180ff
abhishekgarfield <abhishek2759@gmail.com>
2022-08-17 20:04:25 +0530

    calc deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home"
 
 const App=()=>{
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Home />}/>
             </Routes>
